61539e8ec8ad70dacbcdb2640388749c241f2d70
Jefferson Maylon Zickuhr <jefmaylon@gmail.com>
2021-02-06 13:10:14 -0300

    TerceiroCommit

---
+++ Fevereiro/src/Av1/CoronaCategoria.java
@@ -0,0 +1,14 @@
+package Av1;
+
+public enum CoronaCategoria {
+
+	EMPREGADO("Empregado"),
+	EMPREGADOR("Empregador"),
+	DESEMPREGADO("Desempregado");
+
+	private String categoria;
+	
+	CoronaCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+}
