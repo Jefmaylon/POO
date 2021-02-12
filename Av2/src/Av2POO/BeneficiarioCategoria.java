dd79544aed3ebd84ee9c86215c6cffe697f05947
Jefferson Maylon Zickuhr <jefmaylon@gmail.com>
2021-02-11 21:11:17 -0300

    Avaliação 2

---
+++ Av2/src/Av2POO/BeneficiarioCategoria.java
@@ -0,0 +1,5 @@
+package Av2POO;
+
+public class BeneficiarioCategoria {
+
+}
---
+++ Av2/src/Av2POO/BeneficiarioMain.java
@@ -0,0 +1,10 @@
+package Av2POO;
+
+public class BeneficiarioMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
